148e5af8d43f16e2bc012552808698153b6738b5
jadencamelot <10377013+jadencamelot@users.noreply.github.com>
2023-10-04 02:22:38 +1100

    Merge defaults.sh into osx-config.sh

--- mac/defaults.sh
+++
@@ -1,4 +0,0 @@
-!#/usr/bin/env bash
-
-# Remove "bold look" fonts for Alacritty on MacOS 11 and later - see https://github.com/alacritty/alacritty/issues/4616
-defaults write org.alacritty AppleFontSmoothing -int 0
--- mac/osx-config.sh
+++ mac/osx-config.sh
@@ -18,6 +18,9 @@
 # Auto-hide menu bar
 #defaults write NSGlobalDomain _HIHideMenuBar -bool YES
 
+# Remove "bold look" fonts for Alacritty on MacOS 11 and later - see https://github.com/alacritty/alacritty/issues/4616
+defaults write org.alacritty AppleFontSmoothing -int 0
+
 ### Menu Bar
 
 # Show these icons in the menu bar
