fa8476d6cbbd593b71152bdcbd556567f7fd9959
jadencamelot <10377013+jadencamelot@users.noreply.github.com>
2023-10-25 18:03:57 +1100

    Add Homebrew to PATH on Apple Silicon

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,9 @@
+# Add installed brew packages to PATH
+if test -e /opt/homebrew/bin/brew
+    # Apple Silicon only - Intel Macs have homebrew in a different location
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
 if status is-interactive
     set -U fish_greeting
 end
